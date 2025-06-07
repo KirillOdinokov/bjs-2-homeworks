1b293a4411a007c61ab892c22fbdb4f17f2c9f24
Kirill Odinokov <odinokov.k@yandex.ru>
2025-06-07 12:25:05 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,44 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	var discriminant = b * b - 4 * a * c;
+	if (a === 0 && b !== 0) {
+		const root = -c / b;
+		arr.push(root);
+	} else if (a === 0 && b === 0 && c !== 0) {
+		const root = 'Уравнение не имеет решений'
+		arr.push(root);
+	} else if (discriminant < 0) {
+		const root = 'Уравнение не имеет корней'
+		arr.push(root);
+	} else if (discriminant === 0) {
+		const root = -b / (2 * a);
+		arr.push(root);
+	} else {
+		const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(root1, root2);
+	}
+	return arr;
 }
 
+console.log(solveEquation(1, 3, 4));
+console.log(solveEquation(22, 44, 33));
+console.log(solveEquation(22, -55, 33));
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	if (countMonths <= 0 || amount <= 0 || percent < 0 || contribution < 0 || contribution > amount) {
+		return 'Проверьте корректность введенных данных'
+	} else {
+		const p = percent / 100 / 12;
+		a = amount - contribution;
+		const summ = (a * (p + (p / (((1 + p) ** countMonths) - 1))));
+		return summ * countMonths;
+	}
 }
+
+console.log(calculateTotalMortgage(10, 0, 50000, 12));
+console.log(calculateTotalMortgage(10, 1000, 50000, 12));
+console.log(calculateTotalMortgage(10, 0, 20000, 24));
+console.log(calculateTotalMortgage(10, 1000, 20000, 24));
